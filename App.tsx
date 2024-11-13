63c34a11f96384e4a0cbe64c882b77401c7a3d2c
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-11-13 19:03:02 -0300

    Organizando o projeto por pastas.

--- App.tsx
+++ App.tsx
@@ -1,26 +1,8 @@
-import { Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={{
-      flex: 1,
-      backgroundColor: "#131016",
-      padding: 24
-    }}>
-      <Text key="1" style={{
-        color: '#fdfdfd',
-        fontSize: 36,
-        fontWeight: 'bold',
-        marginTop: 48,
-      }}>
-        Nome do evento
-      </Text>
-      <Text key="2" style={{
-        color: '#6b6b6b',
-        fontSize: 16,
-      }}>
-        Quarta-feira, 13 de novembro de 2022
-      </Text>
-    </View>
+import Home from './src/screens/Home';
+
+export default function App(){
+  return(
+    <Home />
   );
 }
